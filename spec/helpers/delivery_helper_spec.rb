7e095a2b196f2642e842d39105296fff6f693f56
Brent Kirby <brent@kurbmedia.com>
2012-02-03 01:11:32 -0500

    skip email notifications on travis

--- spec/helpers/delivery_helper_spec.rb
+++ spec/helpers/delivery_helper_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 
 describe DeliveryHelper, :type => :helper do
-
-  Transit::Delivery.configure(:audio) do |context, tpl|
-    content_tag(:div, "", :class =>  "audio-player", 
-      :data => { 
-        :context_id   => context.id.to_s, 
-        :context_type => "audio", 
-        :source       => context.source 
-      }).html_safe
-  end
   
   before(:all) do
     @post = Post.make!
     @text  = @post.contexts.build({ :position => 0, :body => "<p>sample text body</p>" }, TextBlock)
     @audio = @post.contexts.build({ :position => 1, :source => "somefile.mp3" }, Audio)
     @post.save
+  end
+  Audio
+  
+  before do
+    Transit::Delivery.configure(:audio) do |context|
+      content_tag(:div, "", :class =>  "audio-player", 
+        :data => { 
+          :context_id   => context.id.to_s, 
+          :context_type => "audio", 
+          :source       => context.source 
+        }).html_safe
+    end
   end
   
   let(:body_copy) do
